a4f20fb1ee306e4abb304393e0de78ac94c4b52c
isnifer <isnifer@gmail.com>
2013-07-12 15:26:05 +0400

    Comments and fixes to main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 (function(){
 
-    var checkBrowser = function(){
+        // Показывает текст, если по какой-то причине нет иконок браузеров
+    var viewHiddenText = function(){
         var arr = document.getElementsByClassName('hide'),
             arrLength = arr.length,
             i = 0;
@@ -10,16 +11,17 @@
             }
 
     },
-        xhr = new XMLHttpRequest(),
+        xhr,
         progressBar,
         testImg = document.getElementsByClassName('test')[0],
         logo = document.getElementsByClassName('logo')[0],
+        // Получаем url изображения иконок браузеров
         getBackgroundImage = function (elem) {
 
             var style = window.getComputedStyle(elem),
                 backgroundImage = style.backgroundImage;
 
-            backgroundImage =  backgroundImage.substring(backgroundImage.search('img'), backgroundImage.indexOf('png'));
+            backgroundImage = backgroundImage.substring(backgroundImage.search('img'), backgroundImage.indexOf('png'));
             backgroundImage = backgroundImage + 'png';
 
             return backgroundImage;
@@ -28,21 +30,23 @@
 
     // Проверяем в наличии ли картинка на странице
     if (!testImg.width){
-        checkBrowser();
+        viewHiddenText();
     }
 
 
-    // GET
+    // Проверяем доступны ли иконки браузеров
+    xhr = new XMLHttpRequest();
     xhr.open('GET', getBackgroundImage(logo), true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
             if (xhr.status !== 200) {
-                checkBrowser();
+                viewHiddenText();
             }
         }
     };
     xhr.send(null);
 
+    // Обновляем ProgressBar
     progressBar = setInterval(function(){
 
         var bar = document.getElementsByClassName('bar')[0],
@@ -54,9 +58,7 @@
         barWidth += 1;
 
         percents = Math.floor(barWidth);
-
         bar.style.width = barWidth + '%';
-
         digit.innerHTML = percents;
 
         if (percents > 99){
